e8f315a1ff43d8559fef1167c9b58ed052665d34
wmwaqas <wmwaqasmahmood@hotmail.com>
2017-08-11 03:22:40 +0200

    generate authentication

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const students = require('./students/students.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(students);
+  app.configure(users);
 };
