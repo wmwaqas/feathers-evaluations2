ab688ea7a689846ea95c7a67c85a094c57766f77
wmwaqas <wmwaqasmahmood@hotmail.com>
2017-08-11 03:19:41 +0200

    generate students service

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
+const students = require('./students/students.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(students);
 };
